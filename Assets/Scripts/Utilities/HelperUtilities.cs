bbd1de07ccbdaf06a5ad3ff89a5e55a5122ddea1
Dana Sharon <whosedreamisthis@gmail.com>
2026-01-14 14:09:40 -0800

    Room Node Types

--- Assets/Scripts/Utilities/HelperUtilities.cs
+++ Assets/Scripts/Utilities/HelperUtilities.cs
@@ -19,10 +19,13 @@ public static class HelperUtilities
             return true;
         }
         return false;
-
     }
 
-    public static bool ValidateCheckEnumerableValues(Object thisObject, string fieldName, IEnumerable enumerableObjectToCheck)
+    public static bool ValidateCheckEnumerableValues(
+        Object thisObject,
+        string fieldName,
+        IEnumerable enumerableObjectToCheck
+    )
     {
         bool error = false;
         int count = 0;
@@ -34,14 +37,16 @@ public static class HelperUtilities
                 Debug.Log(fieldName + " has null values in object " + thisObject.name.ToString());
                 error = true;
             }
-            else { count++; }
+            else
+            {
+                count++;
+            }
         }
 
         if (count == 0)
         {
-            Debug.Log(fieldName + " has no values in object " + thisObject.name.ToString())
+            Debug.Log(fieldName + " has no values in object " + thisObject.name.ToString());
             error = true;
-
         }
         return error;
     }
