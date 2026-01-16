b6ef83b89f3ac26ee644b7e0c1d8f7c0211c7294
Dana Sharon <whosedreamisthis@gmail.com>
2026-01-16 11:01:38 -0800

    room assets

--- Assets/Scripts/Dungeon/RoomTemplateSO.cs
+++ Assets/Scripts/Dungeon/RoomTemplateSO.cs
@@ -1,100 +1,107 @@
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Room_", menuName = "Scriptable Objects/Dungeon/Room")]
 public class RoomTemplateSO : ScriptableObject
 {
-    [HideInInspector]
-    public string guid;
+    [HideInInspector] public string guid;
 
     #region Header ROOM PREFAB
 
     [Space(10)]
     [Header("ROOM PREFAB")]
+
     #endregion Header ROOM PREFAB
 
-    #region ToolTip
-    [Tooltip(
-        "The gameobject prefab for the room (this will contain all the tilemaps for the room and environment game objects)"
-    )]
-    #endregion ToolTip
+    #region Tooltip
+
+    [Tooltip("The gameobject prefab for the room (this will contain all the tilemaps for the room and environment game objects")]
+
+    #endregion Tooltip
 
     public GameObject prefab;
 
-    [HideInInspector]
-    public GameObject previousPrefab;
+    [HideInInspector] public GameObject previousPrefab; // this is used to regenerate the guid if the so is copied and the prefab is changed
+
 
     #region Header ROOM CONFIGURATION
 
     [Space(10)]
     [Header("ROOM CONFIGURATION")]
+
     #endregion Header ROOM CONFIGURATION
 
-    #region ToolTip
-    [Tooltip(
-        "The room node type SO. The room node types correspond to the room nodes used in the room node graph. The exceptions being with corridors. Un the room node graph there is just one corridor type 'Corridor'. For the room templates there are 2 corridor node types: CorridorNS and CorridorEW"
-    )]
-    #endregion ToolTip
+    #region Tooltip
+
+    [Tooltip("The room node type SO. The room node types correspond to the room nodes used in the room node graph.  The exceptions being with corridors.  In the room node graph there is just one corridor type 'Corridor'.  For the room templates there are 2 corridor node types - CorridorNS and CorridorEW.")]
+
+    #endregion Tooltip
+
     public RoomNodeTypeSO roomNodeType;
 
-    #region ToolTip
-    [Tooltip(
-        "If you imagine a rectange around the room tilemap that just completely encloses it, the room lower bounds represent the bottom left corner of hte rectange. this should be determined from the tilemap for the room (using the coordinate bursh pointer to get the tilemap grid position)"
-    )]
-    #endregion ToolTip
+    #region Tooltip
+
+    [Tooltip("If you imagine a rectangle around the room tilemap that just completely encloses it, the room lower bounds represent the bottom left corner of that rectangle. This should be determined from the tilemap for the room (using the coordinate brush pointer to get the tilemap grid position for that bottom left corner (Note: this is the local tilemap position and NOT world position")]
+
+    #endregion Tooltip
 
     public Vector2Int lowerBounds;
 
-    #region ToolTip
-    [Tooltip(
-        "If you imagine a rectange around the room tilemap that just completely encloses it, the room upper bounds represent the top right corner of the rectange. this should be determined from the tilemap for the room (using the coordinate bursh pointer to get the tilemap grid position)"
-    )]
-    #endregion ToolTip
+    #region Tooltip
+
+    [Tooltip("If you imagine a rectangle around the room tilemap that just completely encloses it, the room upper bounds represent the top right corner of that rectangle. This should be determined from the tilemap for the room (using the coordinate brush pointer to get the tilemap grid position for that top right corner (Note: this is the local tilemap position and NOT world position")]
+
+    #endregion Tooltip
 
     public Vector2Int upperBounds;
 
-    #region ToolTip
-    [Tooltip(
-        "There should be a maximum of four doorways for a room - one for each compass direction. These should havea consistent 3 tile opening"
-    )]
-    #endregion ToolTip
+    #region Tooltip
 
-    [SerializeField]
-    public List<Doorway> doorwayList;
+    [Tooltip("There should be a maximum of four doorways for a room - one for each compass direction.  These should have a consistent 3 tile opening size, with the middle tile position being the doorway coordinate 'position'")]
 
-    #region ToolTip
-    [Tooltip(
-        "Each possible spawn position (used for enemies and chests) for the room in tilemap coordinate should be added to this array"
-    )]
-    #endregion ToolTip
+    #endregion Tooltip
+
+    [SerializeField] public List<Doorway> doorwayList;
+
+    #region Tooltip
+
+    [Tooltip("Each possible spawn position (used for enemies and chests) for the room in tilemap coordinates should be added to this array")]
+
+    #endregion Tooltip
 
     public Vector2Int[] spawnPositionArray;
 
+    /// <summary>
+    /// Returns the list of Entrances for the room template
+    /// </summary>
     public List<Doorway> GetDoorwayList()
     {
         return doorwayList;
     }
 
     #region Validation
+
 #if UNITY_EDITOR
 
+    // Validate SO fields
     private void OnValidate()
     {
+        // Set unique GUID if empty or the prefab changes
         if (guid == "" || previousPrefab != prefab)
         {
             guid = GUID.Generate().ToString();
             previousPrefab = prefab;
             EditorUtility.SetDirty(this);
         }
+
         HelperUtilities.ValidateCheckEnumerableValues(this, nameof(doorwayList), doorwayList);
-        HelperUtilities.ValidateCheckEnumerableValues(
-            this,
-            nameof(spawnPositionArray),
-            spawnPositionArray
-        );
+
+        // Check spawn positions populated
+        HelperUtilities.ValidateCheckEnumerableValues(this, nameof(spawnPositionArray), spawnPositionArray);
     }
+
 #endif
+
     #endregion Validation
 }
