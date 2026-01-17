f49f924b821ca4c0f54aaba0123f30883780bca5
Dana Sharon <whosedreamisthis@gmail.com>
2026-01-17 12:23:19 -0800

    Room class

---
+++ Assets/Scripts/Dungeon/Room.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room
+{
+    public string id;
+    public string templateID;
+    public GameObject prefab;
+    public RoomNodeTypeSO roomNodeType;
+    public Vector2Int lowerBounds;
+    public Vector2Int upperBounds;
+    public Vector2Int templateLowerBounds;
+    public Vector2Int templateUpperBounds;
+    public Vector2Int spawnPositionArray;
+    public List<string> childRoomIDList;
+    public string parentRoomID;
+    public List<Doorway> doorwayList;
+    public bool isPositioned = false;
+    public InstantiatedRoom istantiatedRoom;
+    public bool isLit = false;
+    public bool isClearedOffEnemies = false;
+    public bool isPreviouslyVisited = false;
+
+    public Room()
+    {
+        childRoomIDList = new List<string>();
+        doorwayList = new List<Doorway>();
+    }
+}
